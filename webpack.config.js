6eea8744fc000b118fdc8450edc8eee4f6af2b1d
tc30nguyen <tc30nguyen@gmail.com>
2016-03-07 15:02:01 -0800

    Initial add SASS Fix webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   entry: './server/server.js',
   target: 'node',
+  output: {
+    path: __dirname + '/public/js',
+    filename: "bundle.js"
+  },
   module: {
     loaders: [
       { test: /\.css$/, loader: 'style!css' }
